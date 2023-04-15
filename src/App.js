f8bd80417049ed5d48e59d701a48ab708d132a36
1daniyarovaa <attokurovadiana9@gmail.com>
2023-04-15 15:39:06 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './Layout/Layout';
 import Home from './pages/Home';
+import Promotion from './pages/Promotion/Promotion';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Layout/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/promotion' element={<Promotion />}/>
       </Routes>
     </>
   );
