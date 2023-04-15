dc660b854e00f94ee43e1f56670a18803c4edacc
1daniyarovaa <attokurovadiana9@gmail.com>
2023-04-15 15:08:57 +0300

    train

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from "./Header.module.scss"
+import img1 from '../../assets/strelka.svg'
+const Header = () => {
+  return (
+    <div className={style.Header}>
+        <div className={style.headerText}>
+            <p>Главная</p>
+            <img src={img1} alt="" />
+            <p>Акции</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
