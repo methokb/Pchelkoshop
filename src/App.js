bf74276ad71480070abde3d8ae706d3b399f8683
methokb <abdurasulovrahmatso@gmail.com>
2023-04-15 15:39:30 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './Layout/Layout';
-import Home from './pages/Home';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Layout/>
+      <Layout />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
